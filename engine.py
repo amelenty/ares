afe6ee174abde1442fa9fdfc6041d8224ec2510c
amelentyeva <amelentyeva@rubiconproject.com>
2018-10-17 11:20:11 +0300

    :art: refactor key handling into a separate module

--- engine.py
+++ engine.py
@@ -1,5 +1,7 @@
 import tcod
 
+from handle_keys import handle_keys
+
 
 def main():
     screen_width = 80
@@ -10,29 +12,35 @@ def main():
 
     tcod.console_set_custom_font('arial10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
 
-    console = tcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    console = tcod.console_init_root(screen_width, screen_height, 'tcod tutorial revised', False)
+
+    key = tcod.Key()
+    mouse = tcod.Mouse()
 
     while not tcod.console_is_window_closed():
+        tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
+
         tcod.console_set_default_foreground(console, tcod.white)
         tcod.console_put_char(console, player_x, player_y, '@', tcod.BKGND_NONE)
         tcod.console_flush()
 
-        key = tcod.console_check_for_keypress()
+        action = handle_keys(key)
 
-        if key.vk == tcod.KEY_UP:
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
             tcod.console_put_char(console, player_x, player_y, ' ', tcod.BKGND_NONE)
-            player_y -= 1
-        elif key.vk == tcod.KEY_DOWN:
-            tcod.console_put_char(console, player_x, player_y, ' ', tcod.BKGND_NONE)
-            player_y += 1
-        elif key.vk == tcod.KEY_LEFT:
-            tcod.console_put_char(console, player_x, player_y, ' ', tcod.BKGND_NONE)
-            player_x -= 1
-        elif key.vk == tcod.KEY_RIGHT:
-            tcod.console_put_char(console, player_x, player_y, ' ', tcod.BKGND_NONE)
-            player_x += 1
-        elif key.vk == tcod.KEY_ESCAPE:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
             return True
+
+        if fullscreen:
+            tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
 
 
 if __name__ == '__main__':
