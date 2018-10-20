7b9c66e65007ff1b73c9aaf3b9e2fd17db86eb14
amelentyeva <amelentyeva@rubiconproject.com>
2018-10-20 17:19:24 +0300

    :sparkles: add field of view

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import tcod
 
 from entity import Entity
+from fov_functions import initialize_fov, recompute_fov
 from input_handlers import handle_keys
 from render import render_all, clear_all
 from world.game_map import GameMap
@@ -16,9 +17,15 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    fov_algorithm = 0
+    fov_light_walls = False
+    fov_radius = 10
+
     colors = {
         'dark_wall': tcod.darkest_gray,
-        'dark_ground': tcod.darker_gray
+        'dark_ground': tcod.darker_gray,
+        'light_wall': tcod.darkest_gray,
+        'light_ground': tcod.dark_gray
     }
 
     player = Entity(map_width // 2, map_height // 2, '@', tcod.green)
@@ -32,13 +39,21 @@ def main():
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
+    fov_recompute = True
+    fov_map = initialize_fov(game_map)
+
     key = tcod.Key()
     mouse = tcod.Mouse()
 
     while not tcod.console_is_window_closed():
         tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(console, entities, game_map, screen_width, screen_height, colors)
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+
+        render_all(console, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+
+        fov_recompute = False
 
         tcod.console_flush()
 
@@ -54,6 +69,7 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+                fov_recompute = True
 
         if exit:
             return True
--- render.py
+++ render.py
@@ -4,36 +4,46 @@ OFFSET_X = 5
 OFFSET_Y = 0
 
 
-def render_all(console, entities, game_map, screen_width, screen_height, colors):
-    # Draw all the tiles in the game map
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            wall = game_map.tiles[x][y].block_sight
+def render_all(console, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors):
+    if fov_recompute:
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                visible = tcod.map_is_in_fov(fov_map, x, y)
+                wall = game_map.tiles[x][y].block_sight
 
-            if wall:
-                tcod.console_set_char_background(console, OFFSET_X + x, OFFSET_Y + y, colors.get('dark_wall'),
-                                                 tcod.BKGND_SET)
-            else:
-                tcod.console_set_char_background(console, OFFSET_X + x, OFFSET_Y + y, colors.get('dark_ground'),
-                                                 tcod.BKGND_SET)
+                ox = OFFSET_X + x
+                oy = OFFSET_Y + y
+
+                if visible:
+                    if wall:
+                        tcod.console_set_char_background(console, ox, oy, colors.get('light_wall'), tcod.BKGND_SET)
+                    else:
+                        tcod.console_set_char_background(console, ox, oy, colors.get('light_ground'), tcod.BKGND_SET)
+                    game_map.tiles[x][y].explored = True
+                elif game_map.tiles[x][y].explored:
+                    if wall:
+                        tcod.console_set_char_background(console, ox, oy, colors.get('dark_wall'), tcod.BKGND_SET)
+                    else:
+                        tcod.console_set_char_background(console, ox, oy, colors.get('dark_ground'), tcod.BKGND_SET)
 
     # Draw all entities in the list
     for entity in entities:
-        draw_entity(console, entity)
+        draw_entity(console, entity, fov_map)
 
     tcod.console_blit(console, 0, 0, screen_width, screen_height, 0, 0, 0)
 
 
-def clear_all(con, entities):
+def clear_all(console, entities):
     for entity in entities:
-        clear_entity(con, entity)
+        clear_entity(console, entity)
 
 
-def draw_entity(con, entity):
-    tcod.console_set_default_foreground(con, entity.color)
-    tcod.console_put_char(con, OFFSET_X + entity.x, OFFSET_Y + entity.y, entity.char, tcod.BKGND_NONE)
+def draw_entity(console, entity, fov_map):
+    if fov_map.fov[entity.y][entity.x]:
+        tcod.console_set_default_foreground(console, entity.color)
+        tcod.console_put_char(console, OFFSET_X + entity.x, OFFSET_Y + entity.y, entity.char, tcod.BKGND_NONE)
 
 
-def clear_entity(con, entity):
+def clear_entity(console, entity):
     # erase the character that represents this object
-    tcod.console_put_char(con, OFFSET_X + entity.x, OFFSET_Y + entity.y, ' ', tcod.BKGND_NONE)
+    tcod.console_put_char(console, OFFSET_X + entity.x, OFFSET_Y + entity.y, ' ', tcod.BKGND_NONE)
